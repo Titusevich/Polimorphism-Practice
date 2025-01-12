25c38dea0dc2a08c77d9e57b38a4a5cc9f54d289
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-12 23:30:38 +0200

    Add new method calcTotalSquare in class AppService

--- src/app/service/AppService.java
+++ src/app/service/AppService.java
@@ -1,4 +1,14 @@
 package app.service;
 
+import app.model.IGeometricShape;
+
 public class AppService {
+
+    public double calcTotalSquare(IGeometricShape[] shapes) {
+        double totalSquare = 0;
+        for (IGeometricShape shape : shapes) {
+            totalSquare += shape.calcSquare();
+        }
+        return totalSquare;
+    }
 }
