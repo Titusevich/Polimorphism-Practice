76a40985ee54a13900694446d13bfb145601157c
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-12 23:22:18 +0200

    Add geometric figures info with Scanner

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,21 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the radius of circle: ");
+        double radius = scan.nextDouble();
+        System.out.println("Enter the first side of triangle, cm: ");
+        double triangleSide1 = scan.nextDouble();
+        System.out.println("Enter the second side of triangle, cm: ");
+        double triangleSide2 = scan.nextDouble();
+        System.out.println("Enter the third side of triangle, cm: ");
+        double triangleSide3 = scan.nextDouble();
+        System.out.println("Enter the side of quadrant: ");
+        double quadrantSide = scan.nextDouble();
     }
 }
